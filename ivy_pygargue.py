d090befccf51a2951a70804a68f37d41306b0581
EngelOfChipolata <engelofdeath@live.fr>
2017-12-30 21:54:38 +0100

    Updates robot position updates

--- ivy_pygargue.py
+++ ivy_pygargue.py
@@ -8,7 +8,7 @@ DEFAULT_BUS = '127.255.255.255:2010'
 
 NEW_POLYGON_OBSTACLE_REGEXP = "New Obstacle id : (.*) type : POLYGON points : (.*)"  # eg : New Obstacle id : 3 type : POLYGON points : 1500,350;1500,650;1000,650;1000,350
 NEW_CIRCLE_OBSTACLE_REGEXP = "New Obstacle id : (.*) type : CIRCLE center : (.*) radius : (.*)"  # eg : New Obstacle id : 6 type : CIRCLE center : 500,800 radius : 150
-ROBOT_POSITION_REGEXP = "Robot position (.*) orientation (.*)"  # eg : Robot position 552,789 orientation 3.14159265
+UPDATE_ROBOT_POSITION_REGEXP = "Update robot pose (.*)"  # eg : Update robot pose 325;1523;-1.57785
 NEW_TRAJECTORY_REGEXP = "New trajectory (.*)"  # eg : New trajectory 528,450;1200,564;846,1486
 GO_TO_REGEXP = "Go to {},{}"
 
@@ -19,7 +19,7 @@ class Ivy:
         IvyStart(bus)
         IvyBindMsg(self.on_new_polygon_obstacle, NEW_POLYGON_OBSTACLE_REGEXP)
         IvyBindMsg(self.on_new_circle_obstacle, NEW_CIRCLE_OBSTACLE_REGEXP)
-        IvyBindMsg(self.on_new_robot_position, ROBOT_POSITION_REGEXP)
+        IvyBindMsg(self.on_new_robot_position, UPDATE_ROBOT_POSITION_REGEXP)
         IvyBindMsg(self.on_new_trajectory, NEW_TRAJECTORY_REGEXP)
 
     def on_new_polygon_obstacle(self, agent, *arg):
@@ -39,8 +39,8 @@ class Ivy:
         self.app.repaint()
 
     def on_new_robot_position(self, agent, *arg):
-        x, y = arg[0].split(',')
-        self.app.move_robot(float(x), float(y), float(arg[1]))
+        x, y, theta = arg[0].split(';')
+        self.app.move_robot(float(x), float(y), float(theta))
 
     def on_new_trajectory(self, agent, *arg):
         trajectory = []

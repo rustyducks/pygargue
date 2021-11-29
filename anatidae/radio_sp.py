713bce67315a77daae0a7679cc8baa927c2a053c
Fabien-B <fabien.bonneval@gmail.com>
2021-11-29 19:47:04 +0100

    Move encoding/decoding to Transport class.

--- anatidae/radio_sp.py
+++ anatidae/radio_sp.py
@@ -1,22 +1,9 @@
 import time
-from PyQt5.QtCore import *
 from serial import Serial
-from enum import Enum
-import struct
-from generated import messages_pb2 as m
-
-from google import protobuf
 from robot import *
-from utils import *
 from abstract_radio import AbstractRadio, filter_sender
 from logger import Logger
-
-
-class RcvState(Enum):
-    START1 = 0
-    START2 = 1
-    LEN = 2
-    PAYLOAD_CHK = 3
+from transport import Transport
 
 
 class RadioSP(QThread, AbstractRadio):
@@ -27,8 +14,7 @@ class RadioSP(QThread, AbstractRadio):
     def __init__(self, port, baudrate, logger: Logger, rid="dafi", name="Ana", parent=None):
         super().__init__(logger=logger, parent=parent)
         self.serial = Serial(port, baudrate, timeout=0.01)
-        self._rcv_state = RcvState.START1
-        self._nb_bytes_expected = 1
+        self.transport = Transport()
         self.stop_requested = False
         self.rid = rid
         self.add_rid(rid)
@@ -38,47 +24,14 @@ class RadioSP(QThread, AbstractRadio):
         print("stopping...")
         self.requestInterruption()
         while self.isRunning():
-            time.sleep(0.001)
+            time.sleep(0.01)
             pass
         self.serial.close()
 
-    @staticmethod
-    def compute_chk(payload):
-        chk = 0
-        for b in payload:
-            chk ^= b
-        return chk
-
     def check_msgs(self):
-        while self.serial.in_waiting >= self._nb_bytes_expected:
-            if self._rcv_state == RcvState.START1:  # wait for 0XFF
-                if ord(self.serial.read()) == 0xFF:
-                    self._rcv_state = RcvState.START2
-                    self._nb_bytes_expected = 1
-            elif self._rcv_state == RcvState.START2:
-                if ord(self.serial.read()) == 0xFF:
-                    self._rcv_state = RcvState.LEN
-                    self._nb_bytes_expected = 1
-                else:  # fallback to Idle
-                    self._rcv_state = RcvState.START1
-                    self._nb_bytes_expected = 1
-            elif self._rcv_state == RcvState.LEN:
-                self._nb_bytes_expected = ord(self.serial.read()) + 1  # expect 1 more byte for the checksum
-                self._rcv_state = RcvState.PAYLOAD_CHK
-            elif self._rcv_state == RcvState.PAYLOAD_CHK:
-                payload = self.serial.read(self._nb_bytes_expected - 1)  # read message content
-                chk = ord(self.serial.read())
-                self._rcv_state = RcvState.START1
-                if chk == self.compute_chk(payload):
-                    msg = m.Message()
-                    try:
-                        msg.ParseFromString(payload)
-                        return msg
-                    except protobuf.message.DecodeError:
-                        return None
-                else:
-                    print(f"chgk failed: {chk}  {self.compute_chk(payload)}")
-                    return None
+        data = self.serial.read(self.serial.in_waiting)
+        msg = self.transport.put(data)
+        return msg
 
     def handle_umsg(self, msg):
         self.emit_msg(self.rid, msg)
@@ -97,10 +50,7 @@ class RadioSP(QThread, AbstractRadio):
 
     @filter_sender
     def send_msg(self, rid, msg: m.Message):
-        payload = msg.SerializeToString()
-        header = struct.pack("<BBB", 0xFF, 0xFF, len(payload))
-        chk = struct.pack("<B", self.compute_chk(payload))
-        data = header + payload + chk
+        data = self.transport.serialize(msg)
         self.serial.write(data)
         self.serial.flushOutput()
         d = self.msg_to_dict(self.name, msg)
